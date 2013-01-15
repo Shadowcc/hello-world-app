30762bdad6e677e857cd9ba0eaf3dfbc9cd99f96
Scott Monroe <sdmonroe@SH308-13.local>
2013-01-15 11:37:57 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldViewController.m
@@ -0,0 +1,34 @@
+//
+//  HelloWorldViewController.m
+//  HelloWorld
+//
+//  Created by default on 1/15/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
